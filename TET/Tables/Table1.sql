c7896836a202f718a8edfbf34c0b39eaf543c8bb
OvidiuEDSgrup <OvidiuEDSgrup@users.noreply.github.com>
2016-06-21 15:31:44 +0300

    CReat tabela detalii note credit

---
+++ TET/Tables/Table1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[IdPozDoc] INT NOT NULL PRIMARY KEY, 
+	subunit varchar(9),
+    [furnizor] VARCHAR(13) NULL, 
+    [numar_NC] VARCHAR(8) NULL, 
+    [data] DATETIME2(0) NULL, 
+    [cod] VARCHAR(20) NULL, 
+    [cantitate] DECIMAL(12, 3) NULL, 
+    [pret] DECIMAL(15, 5) NULL, 
+    [pret_valuta] DECIMAL(15, 5) NULL
+)
